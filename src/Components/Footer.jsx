156fd8ed0642866c8ed360866447e9ad46a61cbe
mushfiq0000 <kazi.mushfiq.ehsan@gmail.com>
2025-11-10 05:29:06 +0600

    modified footer from daisyui, added a custom logo and replaced icon using react icons.

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,11 +1,54 @@
-import React from 'react';
+import { FaFacebookF, FaInstagram, FaLinkedinIn } from "react-icons/fa";
+import { FaXTwitter } from "react-icons/fa6";
+import { MdOutlineEmail } from "react-icons/md";
 
 const Footer = () => {
-    return (
+  return (
+    <div>
+      <footer className="footer sm:footer-horizontal text-base-content p-10 shadow">
         <div>
-            Footer
+            <div className="flex items-center gap-3 text-xl font-bold">
+                <img className="w-20" src="https://i.ibb.co.com/5XXH6Dj9/Gemini-Generated-Image-4iszgc4iszgc4isz-removebg-preview.png" alt="" />
+                <p>FinEase</p>
+            </div>
+            <p>
+                Your personal finance management companion.
+                <br />
+                 Track expenses, manage budgets, and achieve your financial goals with ease.
+            </p>
+
+            <div className="flex items-center gap-2">
+            <MdOutlineEmail />
+            <p>contact@finease.com</p>
+            </div>
+
         </div>
-    );
+        <nav>
+          <h6 className="footer-title">Company</h6>
+          <a className="link link-hover">About us</a>
+          <a className="link link-hover">Contact</a>
+          <a className="link link-hover">Jobs</a>
+        </nav>
+        <nav>
+          <h6 className="footer-title">Social</h6>
+          <div className="grid grid-flow-col gap-4">
+            <a>
+              <FaXTwitter />
+            </a>
+            <a>
+              <FaFacebookF />
+            </a>
+            <a>
+              <FaInstagram />
+            </a>
+            <a>
+              <FaLinkedinIn />
+            </a>
+          </div>
+        </nav>
+      </footer>
+    </div>
+  );
 };
 
 export default Footer;
