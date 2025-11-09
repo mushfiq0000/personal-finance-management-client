82fd9e6e206782ec4a4a4366d6382aebdee79645
mushfiq0000 <kazi.mushfiq.ehsan@gmail.com>
2025-11-10 05:02:50 +0600

    make responsive navbar add react-icon and add login and signup button

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,9 +1,114 @@
-import React from 'react';
+import { AiOutlineTransaction } from "react-icons/ai";
+import { IoHomeOutline } from "react-icons/io5";
+import { MdOutlineAddCard } from "react-icons/md";
+import { TbReportSearch } from "react-icons/tb";
+import { Link, NavLink } from "react-router";
 
 const Navbar = () => {
+  const link = (
+    <>
+      <NavLink
+        className={({ isActive }) =>
+          `mr-2 p-2 transition-all  duration-10 ${
+            isActive ? " border-[#c67033]" : ""
+          }`
+        }
+        to="/"
+      >
+        {" "}
+        <div className="flex items-center gap-1">
+          {" "}
+          <IoHomeOutline />
+          Home{" "}
+        </div>{" "}
+      </NavLink>
+      <NavLink
+        className={({ isActive }) =>
+          `mr-2 p-2  transition-all  duration-10 ${
+            isActive ? " border-[#c67033]" : " "
+          }`
+        }
+        to=""
+      >
+        {" "}
+        <div className="flex items-center gap-1">
+          <MdOutlineAddCard /> Add Transaction{" "}
+        </div>{" "}
+      </NavLink>
+      <NavLink
+        className={({ isActive }) =>
+          `mr-2 p-2  transition-all  duration-10 ${
+            isActive ? " border-[#c67033]" : ""
+          }`
+        }
+        to=""
+      >
+        <div className="flex items-center gap-1">
+          <AiOutlineTransaction /> My Transactions
+        </div>
+      </NavLink>
+      <NavLink
+        className={({ isActive }) =>
+          `mr-2 p-2  transition-all  duration-10 ${
+            isActive ? " border-[#c67033]" : ""
+          }`
+        }
+        to=""
+      >
+        <div className="flex items-center gap-1">
+          <TbReportSearch /> Reports
+        </div>
+      </NavLink>
+    </>
+  );
+
   return (
     <div>
-      Navbar
+      <div className="navbar shadow-sm">
+        <div className="navbar-start">
+          <div className="dropdown">
+            <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                {" "}
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M4 6h16M4 12h8m-8 6h16"
+                />{" "}
+              </svg>
+            </div>
+            <ul
+              tabIndex="-1"
+              className="menu menu-sm dropdown-content rounded-box z-1 mt-3 w-52 p-2 shadow"
+            >
+              {link}
+            </ul>
+          </div>
+          <div>
+            <Link to="/" className="flex items-center">
+              <img
+                className="w-15"
+                src="https://i.ibb.co.com/5XXH6Dj9/Gemini-Generated-Image-4iszgc4iszgc4isz-removebg-preview.png"
+                alt="Pet Logo"
+              />
+            </Link>
+          </div>
+        </div>
+        <div className="navbar-center hidden lg:flex">
+          <ul className="menu menu-horizontal px-1">{link}</ul>
+        </div>
+        <div className="navbar-end gap-5">
+          <a className="btn">Login</a>
+          <a className="btn">Sign Up</a>
+        </div>
+      </div>
     </div>
   );
 };
--- src/Layout/HomeLayout.jsx
+++ src/Layout/HomeLayout.jsx
@@ -1,11 +1,24 @@
-import React from 'react';
+import React from "react";
+import Navbar from "../Components/Navbar";
+import { Outlet } from "react-router";
+import Footer from "../Components/Footer";
 
 const HomeLayout = () => {
-    return (
-        <div>
-            Home Layout
-        </div>
-    );
+  return (
+    <div>
+      <header>
+        <Navbar />
+      </header>
+
+      <main>
+        <Outlet />
+      </main>
+
+      <footer>
+        <Footer />
+      </footer>
+    </div>
+  );
 };
 
 export default HomeLayout;
